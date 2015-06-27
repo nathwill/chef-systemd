2b61808ccfd14f9e9adf8b39d3e450d5a15fee42
Nathan Williams <nathan@teamtreehouse.com>
2015-06-27 09:04:44 -0700

    stub systemd_unit resource

---
+++ libraries/resource_systemd_unit.rb
@@ -0,0 +1,53 @@
+
+require_relative 'systemd_helpers'
+
+class Chef::Resource
+  class SystemdUnit < Chef::Resource
+    identity_attr :name
+
+    def initialize(name, run_context = nil)
+      super
+      @name = name
+      @resource_name = :systemd_unit
+      @provider = Chef::Provider::SystemdUnit
+      @allowed_actions = [:create, :delete]
+      @action = :create
+    end
+
+    def type(arg = nil)
+      set_or_return(
+        :type, arg,
+        :required => true,
+        :kind_of => String,
+        :equal_to => %w( service socket device mount automount swap target path timer snapshot slice scope ), # rubocop:disable Metrics/LineLength
+        :default => 'service'
+      )
+    end
+
+    def unit(arg = nil)
+      set_or_return(
+        :unit, arg,
+        :required => true,
+        :kind_of => Array,
+        :callbacks => {
+          'is a valid unit configuration' => lambda do |spec|
+            Systemd::Helpers.validate_config('unit', spec)
+          end
+        }
+      )
+    end
+
+    def install(arg = nil)
+      set_or_return(
+        :install, arg,
+        :required => true,
+        :kind_of => Array,
+        :callbacks => {
+          'is a valid install configuration' => lambda do |spec|
+            Systemd::Helpers.validate_config('install', spec)
+          end
+        }
+      )
+    end
+  end
+end
---
+++ libraries/systemd_helpers.rb
@@ -0,0 +1,9 @@
+
+
+module Systemd
+  module Helpers
+    def self.validate_config(type = nil, config = [])
+      true # TODO: validate configuration directives for unit
+    end
+  end
+end

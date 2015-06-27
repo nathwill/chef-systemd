d6e80d95b7cfa046ba2386581f6e97c4ef8b9a8a
Nathan Williams <nathan@teamtreehouse.com>
2015-06-27 15:53:49 -0700

    add service options

--- libraries/systemd_service.rb
+++ libraries/systemd_service.rb
@@ -3,6 +3,38 @@
 module Systemd
   module Service
     OPTIONS ||= %w(
+      Type=
+      RemainAfterExit=
+      GuessMainPID=
+      PIDFile=
+      BusName=
+      BusPolicy=
+      ExecStart=
+      ExecStartPre=
+      ExecStartPost=
+      ExecReload=
+      ExecStop=
+      ExecStopPost=
+      RestartSec=
+      TimeoutStartSec=
+      TimeoutStopSec=
+      TimeoutSec=
+      WatchdogSec=
+      Restart=
+      SuccessExitStatus=
+      RestartPreventExitStatus=
+      RestartForceExitStatus=
+      PermissionsStartOnly=
+      RootDirectoryStartOnly=
+      NonBlocking=
+      NotifyAccess=
+      Sockets=
+      StartLimitInterval=
+      StartLimitBurst=
+      StartLimitAction=
+      FailureAction=
+      RebootArgument=
+      FileDescriptorStoreMax=
     )
   end
 end

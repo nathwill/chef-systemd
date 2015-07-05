7a28c2624da64c4c2b920fc2f8a187996753167a
Nathan Williams <nathan@teamtreehouse.com>
2015-07-04 21:04:45 -0700

    test other unit sections

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,6 +4,11 @@ describe 'systemd::default' do
   describe file('/etc/systemd/system/test-unit.service') do
     it { should be_file }
     its(:content) { should match /\[Unit\]/ }
-    its(:content) { should match /Documentation/ }
+    its(:content) { should match /Documentation=man:true\(1\)/ }
+    its(:content) { should match /\[Install\]/ }
+    its(:content) { should match /WantedBy=multi-user.target/ }
+    its(:content) { should match /\[Service\]/ }
+    its(:content) { should match /Type=oneshot/ }
+    its(:content) { should match /ExecStart=\/usr\/bin\/true/ }
   end
 end

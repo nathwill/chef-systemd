2aedddf3a23d76e8366885906f3c975efa44b59d
Nathan Williams <nathan@teamtreehouse.com>
2015-07-06 21:03:22 -0700

    fill out metadata

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,15 @@ license 'apache2'
 description 'Installs/Configures systemd'
 long_description 'Installs/Configures systemd'
 version '0.1.0'
+
+%w( rhel centos scientific ).each do |p|
+  supports p, '>= 7'
+end
+
+supports 'debian', '>= 8'
+supports 'ubuntu', '>= 15.04'
+
+unless defined?(Ridley::Chef::Cookbook::Metadata)
+  source_url       'https://github.com/nathwill/chef-systemd'
+  issues_url       'https://github.com/nathwill/chef-systemd/issues'
+end

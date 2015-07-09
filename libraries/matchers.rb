35fcab5da26fb044659fd67c0607d58bb811a8c5
Ranjib Dey <ranjib@pagerduty.com>
2015-07-08 18:52:58 -0700

    satisfy the rubocop gods

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,11 +1,26 @@
 if defined?(ChefSpec)
-  unit_types = %w(automount timer target swap socket device mount path service slice)
+  unit_types = %w(
+    automount
+    timer
+    target
+    swap
+    socket
+    device
+    mount
+    path
+    service
+    slice
+  )
   actions = %w(create delete)
   unit_types.each do |type|
     ChefSpec.define_matcher("systemd_#{type}".to_sym)
     actions.each do |action|
       define_method("#{action}_systemd_#{type}".to_sym) do |resource_name|
-        ChefSpec::Matchers::ResourceMatcher.new("systemd_#{type}".to_sym, action.to_sym, resource_name)
+        ChefSpec::Matchers::ResourceMatcher.new(
+          "systemd_#{type}".to_sym,
+          action.to_sym,
+          resource_name
+        )
       end
     end
   end

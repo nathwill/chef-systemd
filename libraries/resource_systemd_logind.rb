ff2616dc8dba957c93540c9ad31328b23751d20c
Nathan Williams <nath.e.will@gmail.com>
2015-08-07 00:20:46 -0700

    add systemd_logind

---
+++ libraries/resource_systemd_logind.rb
@@ -0,0 +1,19 @@
+require_relative 'resource_systemd_daemon'
+require_relative 'systemd_logind'
+
+class Chef::Resource
+  class SystemdLogind < Chef::Resource::SystemdDaemon
+    self.resource_name = :systemd_logind
+    provides :systemd_logind
+
+    def daemon_type(_ = nil)
+      :logind
+    end
+
+    def label(_ = nil)
+      'Login'
+    end
+
+    option_attributes Systemd::Logind::OPTIONS
+  end
+end

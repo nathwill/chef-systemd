3c7e36a9f61704e69ac411cc3e109cf136d7b832
Nathan Williams <nath.e.will@gmail.com>
2015-08-09 12:34:44 -0700

    remove daemon vestige

--- libraries/provider_systemd_conf.rb
+++ libraries/provider_systemd_conf.rb
@@ -17,14 +17,14 @@ class Chef::Provider
     %i( create delete ).each do |a|
       action a do
         r = new_resource
-        daemon_path = Systemd::Helpers.conf_path(r)
+        conf_path = Systemd::Helpers.conf_path(r)
 
         directory Systemd::Helpers.conf_drop_in_root(r) do
           only_if { r.drop_in }
           not_if { r.action == :delete }
         end
 
-        f = file daemon_path do
+        f = file conf_path do
           content Systemd::Helpers.ini_config(r.to_hash)
           action a
         end

f24bad3d14853663448e30431eb60b1ad4e85141
Nathan Williams <nath.e.will@gmail.com>
2015-08-09 15:23:39 -0700

    provider for base resource

--- libraries/provider_systemd_daemon.rb
+++ libraries/provider_systemd_daemon.rb
@@ -2,6 +2,7 @@ require_relative 'provider_systemd_conf'
 
 class Chef::Provider
   class SystemdDaemon < Chef::Provider::SystemdConf
+    provides :systemd_daemon
     Systemd::Helpers::DAEMONS.each do |daemon|
       provides "systemd_#{daemon}".to_sym
     end
--- libraries/provider_systemd_util.rb
+++ libraries/provider_systemd_util.rb
@@ -2,6 +2,7 @@ require_relative 'provider_systemd_conf'
 
 class Chef::Provider
   class SystemdUtil < Chef::Provider::SystemdConf
+    provides :systemd_util
     Systemd::Helpers::UTILS.each do |util|
       provides "systemd_#{util}".to_sym
     end

0cef1958f32e1d582140bd76fd7b5361c4ff55ac
Nathan Williams <nath.e.will@gmail.com>
2015-08-09 00:06:39 -0700

    add matchers for new resources

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -11,17 +11,47 @@ if defined?(ChefSpec)
     service
     slice
   )
-  actions = %w(create delete enable disable start stop)
-  unit_types.each do |type|
+
+  daemons = %w(
+    journald
+    logind
+    resolved
+    timesyncd
+  )
+
+  actions = %i(create delete )
+  unit_actions = %i( enable disable start stop restart )
+
+  (unit_types | daemons).each do |type|
     ChefSpec.define_matcher("systemd_#{type}".to_sym)
     actions.each do |action|
       define_method("#{action}_systemd_#{type}".to_sym) do |resource_name|
         ChefSpec::Matchers::ResourceMatcher.new(
           "systemd_#{type}".to_sym,
-          action.to_sym,
-          resource_name
+          action, resource_name
         )
       end
+    end
+  end
+
+  unit_types.each do |type|
+    unit_actions.each do |action|
+      define_method("#{action}_systemd_#{type}".to_sym) do |resource_name|
+        ChefSpec::Matchers::ResourceMatcher.new(
+          "systemd_#{type}".to_sym,
+          action, resource_name
+        )
+      end
+    end
+  end
+
+  ChefSpec.define_matcher(:systemd_networkd_link)
+  actions.each do |action|
+    define_method("#{action}_systemd_networkd_link") do |resource_name|
+      ChefSpec::Matchers::ResourceMatcher.new(
+        :systemd_networkd_link,
+        action, resource_name
+      )
     end
   end
 end

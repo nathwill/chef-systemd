e0de6d7706c661e0236d51f3e76cc35507684379
Nathan Williams <nath.e.will@gmail.com>
2015-08-09 18:00:32 -0700

    fix specs

--- spec/unit/recipes/hostname_spec.rb
+++ spec/unit/recipes/hostname_spec.rb
@@ -18,7 +18,7 @@
 
 require 'spec_helper'
 
-describe 'systemd::hostnamed' do
+describe 'systemd::hostname' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new

00b4cd677642b53b0dfed3e44ad022c0fc6c20a6
Nathan Williams <nath.e.will@gmail.com>
2015-08-14 11:14:04 -0700

    simpler actions, docs

--- recipes/sysctl.rb
+++ recipes/sysctl.rb
@@ -16,8 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# A oneshot early boot service to apply
-# sysctl settings under /etc/sysctl.d
 service 'systemd-sysctl' do
   action :enable
 end

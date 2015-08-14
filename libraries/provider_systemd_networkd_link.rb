9739ffb4a5460f3e0aa06c2870978220dbead92d
Nathan Williams <nathan@teamtreehouse.com>
2015-08-13 18:20:42 -0700

    set root dir as const

--- libraries/provider_systemd_networkd_link.rb
+++ libraries/provider_systemd_networkd_link.rb
@@ -22,6 +22,8 @@ require_relative 'helpers'
 
 class Chef::Provider
   class SystemdNetworkdLink < Chef::Provider::LWRPBase
+    DIR = '/etc/systemd/network'
+
     use_inline_resources
 
     def whyrun_supported?
@@ -33,13 +35,12 @@ class Chef::Provider
     %i( create delete ).each do |a|
       action a do
         r = new_resource
-        link_dir = '/etc/systemd/network'
 
-        directory link_dir do
+        directory DIR do
           not_if { r.action == :delete }
         end
 
-        f = file ::File.join(link_dir, "#{r.name}.link") do
+        f = file ::File.join(DIR, "#{r.name}.link") do
           content Systemd::Helpers.ini_config(r.to_hash)
           action a
         end

2bfba51c24139032e762634f4402456da5f55322
Nathan Williams <nathan@teamtreehouse.com>
2015-08-16 21:09:05 -0700

    start filling out exec attributes

--- libraries/systemd_exec.rb
+++ libraries/systemd_exec.rb
@@ -93,4 +93,151 @@ module Systemd
       RuntimeDirectoryMode
     )
   end
+
+  def supplementary_groups(arg = nil)
+    set_or_return(
+      :supplementary_groups, arg,
+      kind_of: Array
+    )
+  end
+
+  def nice(arg = nil)
+    set_or_return(
+      :nice, arg,
+      kind_of: Integer,
+      equal_to: -20.upto(19).to_a
+    )
+  end
+
+  def oom_score_adjust(arg = nil)
+    set_or_return(
+      :oom_score_adjust, arg,
+      kind_of: Integer,
+      equal_to: -1_000.upto(1_000).to_a
+    )
+  end
+
+  def io_scheduling_class(arg = nil)
+    set_or_return(
+      :io_scheduling_class, arg,
+      kind_of: String,
+      equal_to: %w( none realtime best-effort idle )
+    )
+  end
+
+  def io_scheduling_priority(arg = nil)
+    set_or_return(
+      :io_scheduling_priority, arg,
+      kind_of: Integer,
+      equal_to: 0.upto(7).to_a
+    )
+  end
+
+  def cpu_scheduling_policy(arg = nil)
+    set_or_return(
+      :cpu_scheduling_policy, arg,
+      kind_of: String,
+      equal_to: %w( other batch idle fifo rr )
+    )
+  end
+
+  def cpu_scheduling_priority(arg = nil)
+    set_or_return(
+      :cpu_scheduling_priority, arg,
+      kind_of: Integer
+    )
+  end
+
+  def cpu_scheduling_reset_on_fork(arg = nil)
+    set_or_return(
+      :cpu_scheduling_reset_on_fork, arg,
+      kind_of: [TrueClass, FalseClass]
+    )
+  end
+
+  def cpu_affinity(arg = nil)
+    set_or_return(
+      :cpu_affinity, arg,
+      kind_of: Array
+    )
+  end
+
+  def standard_input(arg = nil)
+    set_or_return(
+      :standard_input, arg,
+      kind_of: String,
+      equal_to: %w( null tty tty-force tty-fail socket )
+    )
+  end
+
+  def standard_output(arg = nil)
+    set_or_return(
+      :standard_output, arg,
+      kind_of: String,
+      equal_to: %w(
+        inherit null tty journal syslog
+        kmsg journal+console syslog+console
+        kmsg+console socket
+      )
+    )
+  end
+
+  def standard_error(arg = nil)
+    set_or_return(
+      :standard_error, arg,
+      kind_of: String,
+      equal_to: %w( null tty tty-force tty-fail socket )
+    )
+  end
+
+  def tty_reset(arg = nil)
+    set_or_return(
+      :tty_reset, arg,
+      kind_of: [TrueClass, FalseClass]
+    )
+  end
+
+  def ttyv_hangup(arg = nil)
+    set_or_return(
+      :ttyv_hangup, arg,
+      kind_of: [TrueClass, FalseClass]
+    )
+  end
+
+  def ttyvt_disallocate(arg = nil)
+    set_or_return(
+      :ttyvt_disallocate, arg,
+       kind_of: [TrueClass, FalseClass]
+    )
+  end
+
+  def syslog_level(arg = nil)
+    set_or_return(
+      :syslog_level, arg,
+      kind_of: String,
+      equal_to: %w(
+        kern user mail daemon auth syslog lpr news
+        uucp cron authpriv ftp local0 local1 local2
+        local3 local4 local5 local6 local7
+      )
+    )
+  end
+
+  def syslog_level_prefix(arg = nil)
+    set_or_return(
+      :syslog_level_prefix, arg,
+      kind_of: [TrueClass, FalseClass]
+    )
+  end
+
+  def secure_bits(arg = nil)
+    set_or_return(
+      :secure_bits, arg,
+      kind_of: String,
+      equal_to: %w(
+        keep-caps keep-caps-locked no-setuid-fixup
+        no-setuid-fixup-locked noroot noroot-locked
+      )
+    )
+  end
 end

e63458b16115df51fa34530caf7427bd9d2028fa
Nathan Williams <nathan@teamtreehouse.com>
2015-09-26 12:07:39 -0700

    add systemd_udev_rule provider

---
+++ libraries/provider_systemd_udev_rule.rb
@@ -0,0 +1,71 @@
+#
+# Cookbook Name:: systemd
+# Library:: Chef::Provider::SystemdUdevRule
+#
+# Copyright 2015 The Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'chef/provider/lwrp_base'
+
+class Chef::Provider
+  class SystemdUdevRule < Chef::Provider::LWRPBase
+    DIR ||= '/etc/udev/rules.d'
+
+    use_inline_resources
+
+    def whyrun_supported?
+      true
+    end
+
+    provides :systemd_udev_rule
+
+    %i( create delete ).each do |a|
+      action a do
+        r = new_resource
+
+        directory DIR do
+          not_if { r.action == :delete }
+        end
+
+        f = file ::File.join(DIR, "#{r.name}.rules") do
+          content r.to_s
+          action a
+        end
+
+        new_resource.updated_by_last_action(f.updated_by_last_action?)
+      end
+    end
+
+    action :disable do
+      r = new_resource
+
+      rule_path = ::File.join(DIR, "#{r.name}.rules")
+      null_path = '/dev/null'
+
+      file rule_path do
+        action :delete
+        not_if do
+          ::File.symlink?(rule_path) && ::File.readlink(rule_path) == null_path
+        end
+      end
+
+      l = link rule_path do
+        to null_path
+      end
+
+      new_resource.updated_by_last_action(l.updated_by_last_action?)
+    end
+  end
+end

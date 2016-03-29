e5006abe5a7b8c01de109c6402d5d3fdd9178bd4
Nathan Williams <nathan@teamtreehouse.com>
2016-03-28 19:05:49 -0700

    set up dbus helper class

--- metadata.rb
+++ metadata.rb
@@ -13,7 +13,7 @@ supports 'ubuntu', '>= 15.04'
   supports p, '>= 7.0'
 end
 
-unless defined?(Ridley::Chef::Cookbook::Metadata)
-  source_url       'https://github.com/nathwill/chef-systemd' if respond_to?(:source_url)
-  issues_url       'https://github.com/nathwill/chef-systemd/issues' if respond_to?(:issues_url)
-end
+gem 'ruby-dbus' if respond_to?(:gem)
+
+source_url 'https://github.com/nathwill/chef-systemd' if respond_to?(:source_url)
+issues_url 'https://github.com/nathwill/chef-systemd/issues' if respond_to?(:issues_url)

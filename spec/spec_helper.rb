aa382a1c9ab949367434fc9fc8dc9ae161272054
Nathan Williams <nath.e.will@gmail.com>
2016-09-27 16:32:55 -0700

    set default platform, version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,9 @@ require 'chefspec/berkshelf'
 
 Dir.glob('libraries/*.rb').shuffle.each { |f| require File.expand_path(f) }
 
+RSpec.configure do |config|
+  config.platform = 'centos'
+  config.version = '7.0'
+end
+
 ChefSpec::Coverage.start!

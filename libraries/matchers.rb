763f70278684257d41956fd4a7c21afdd515cd72
Nathan Williams <nath.e.will@gmail.com>
2016-09-27 18:04:34 -0700

    update matchers

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -23,22 +23,20 @@ if defined?(ChefSpec)
   SystemdCookbook::UNITS.each do |type|
     # Define unit matchers
     ChefSpec.define_matcher("systemd_#{type}".to_sym)
-
     Chef::Resource::SystemdUnit.allowed_actions.each do |actn|
       define_method("#{actn}_systemd_#{type}".to_sym) do |resource_name|
         ChefSpec::Matchers::ResourceMatcher.new(
-          "systemd_#{type}".to_sym, actn, resource_name
+          "systemd_#{type}".to_sym, actn.to_sym, resource_name
         )
       end
     end
 
     # Define drop-in matchers
     ChefSpec.define_matcher("systemd_#{type}_drop_in".to_sym)
-
     %w( create delete ).each do |actn|
       define_method("#{actn}_systemd_#{type}_drop_in".to_sym) do |resource_name|
         ChefSpec::Matchers::ResourceMatcher.new(
-          "systemd_#{type}_drop_in".to_sym, actn, resource_name
+          "systemd_#{type}_drop_in".to_sym, actn.to_sym, resource_name
         )
       end
     end
@@ -51,7 +49,7 @@ if defined?(ChefSpec)
     %w( create delete ).each do |actn|
       define_method("#{actn}_systemd_#{daemon}".to_sym) do |resource_name|
         ChefSpec::Matchers::ResourceMatcher.new(
-          "systemd_#{daemon}".to_sym, actn, resource_name
+          "systemd_#{daemon}".to_sym, actn.to_sym, resource_name
         )
       end
     end
@@ -64,9 +62,25 @@ if defined?(ChefSpec)
     %w( create delete ).each do |actn|
       define_method("#{actn}_systemd_#{util}".to_sym) do |resource_name|
         ChefSpec::Matchers::ResourceMatcher.new(
-          "systemd_#{util}".to_sym, actn, resource_name
+          "systemd_#{util}".to_sym, actn.to_sym, resource_name
         )
       end
     end
+  end
+
+  # module actions
+  %w( load unload ).each do |actn|
+    define_method("#{actn}_systemd_modules".to_sym) do |resource_name|
+      ChefSpec::Matchers::ResourceMatcher.new(
+        :systemd_modules, actn.to_sym, resource_name
+      )
+    end
+  end
+
+  # sysctl actions
+  define_method(:apply_systemd_sysctl) do |resource_name|
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :systemd_sysctl, :apply, resource_name
+    )
   end
 end

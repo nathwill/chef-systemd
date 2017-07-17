ee643c0d195d2d9841c2732693874ee376bcb2f7
Nathan Williams <nath.e.will@gmail.com>
2017-07-17 09:41:01 -0700

    we require gem metadata support (#105)

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache-2.0'
 description      'chef cookbook for managing linux systems via systemd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.0.1'
-chef_version     '>= 12' if respond_to?(:chef_version)
+chef_version     '>= 12.8.1' if respond_to?(:chef_version)
 
 depends          'compat_resource'
 

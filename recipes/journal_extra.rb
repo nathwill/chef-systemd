bd9484a83479819231ac902e0daa1404126af07a
Nathan Williams <nath.e.will@gmail.com>
2017-11-13 22:09:07 -0800

    work around https://github.com/Foodcritic/foodcritic/issues/451

--- recipes/journal_extra.rb
+++ recipes/journal_extra.rb
@@ -24,6 +24,11 @@ package 'systemd-journal-gateway' do
 end
 
 package 'systemd-journal-remote' do
-  default_release "#{node['lsb']['codename']}-backports" if platform?('debian')
-  not_if { platform_family?('rhel') }
+  not_if { platform_family?('rhel') || platform?('debian') }
+end
+
+# workaround for https://github.com/Foodcritic/foodcritic/issues/451
+apt_package 'systemd-journal-remote' do
+  default_release "#{node['lsb']['codename']}-backports"
+  only_if { platform?('debian') }
 end

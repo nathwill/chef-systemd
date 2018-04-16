558debd585b5dd225daea2bf921ea723e4a1c78c
Nathan Williams <nath.e.will@gmail.com>
2018-04-16 13:28:04 -0700

    compat_resource deprecated, fix test resource

--- test/fixtures/cookbooks/test/resources/file_toucher.rb
+++ test/fixtures/cookbooks/test/resources/file_toucher.rb
@@ -7,28 +7,28 @@ property :path, String, required: true
 default_action :create
 
 action :create do
-  systemd_service "file-toucher-#{name}" do
+  systemd_service "file-toucher-#{new_resource.name}" do
     service do
       type 'oneshot'
-      exec_start "/bin/touch #{path}"
+      exec_start "/bin/touch #{new_resource.path}"
     end
     action :create
   end
 
-  systemd_timer "file-toucher-#{name}" do
+  systemd_timer "file-toucher-#{new_resource.name}" do
     timer do
-      on_calendar time
+      on_calendar new_resource.time
     end
     action [:create, :enable, :start]
   end
 end
 
 action :delete do
-  systemd_service "file-toucher-#{name}" do
+  systemd_service "file-toucher-#{new_resource.name}" do
     action :delete
   end
 
-  systemd_timer "file-toucher-#{name}" do
+  systemd_timer "file-toucher-#{new_resource.name}" do
     action [:stop, :disable, :delete]
   end
 end
